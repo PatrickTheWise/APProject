8e750631272cae6dcd817401eb0893829cd14f37
Amin <153508080+PatrickTheWise@users.noreply.github.com>
2024-06-11 14:20:12 +0330

    update

---
+++ demo1/demo1/src/main/java/com/example/demo1/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+
+
+    @FXML
+    private void handleMostPriceButton(){
+
+    }
+}
---
+++ demo1/demo1/src/main/java/com/example/demo1/User.java
@@ -0,0 +1,10 @@
+package com.example.demo1;
+
+public class User {
+    String Username;
+    String Firstname;
+    String Lastname;
+    String Email;
+    String PhoneNumber;
+    private String Password;
+    }
