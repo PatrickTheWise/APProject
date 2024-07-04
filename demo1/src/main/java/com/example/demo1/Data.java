4e48b71ba7a61c672976fe03139608908546c0df
amirhossein.abolfazli <amirhosseinabolfazli404@gmail.com>
2024-07-04 22:42:57 +0330

    homepage changes

---
+++ demo1/src/main/java/com/example/demo1/Data.java
@@ -0,0 +1,24 @@
+package com.example.demo1;
+
+public class Data {
+    public static double USDMIN;
+    public static double TMNMIN;
+    public static double EURMIN;
+    public static double YENMIN;
+    public static double GBPMIN;
+    public static double USDMAX;
+    public static double TMNMAX;
+    public static double EURMAX;
+    public static double YENMAX;
+    public static double GBPMAX;
+    public static int[]MInprice = new int[5];
+    public static int[]MAXprice = new int[5];
+    public static double USDPrice;
+    public static double TMNPrice;
+    public static double GBPPrice;
+    public static double EURPrice;
+    public static double YENPrice;
+
+
+
+}
--- demo1/src/main/java/com/example/demo1/Usd.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo1;
-
-public class Usd extends ArzHa{
-    public static int price;
-
-}
