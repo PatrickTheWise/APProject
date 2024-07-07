969ba318ae975ad664d8836e7b145ccffcffbde8
amirhossein.abolfazli <amirhosseinabolfazli404@gmail.com>
2024-07-07 18:32:18 +0330

    swap design complete

--- demo1/src/main/java/com/example/demo1/HelloApplication.java
+++ demo1/src/main/java/com/example/demo1/HelloApplication.java
@@ -10,9 +10,9 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("swap.fxml"));
         //Scene scene = new Scene(fxmlLoader.load(), 390, 440);
-        Scene scene = new Scene(fxmlLoader.load(), 800, 540);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setResizable(false);
--- demo1/src/main/java/com/example/demo1/swapcontroller.java
+++ demo1/src/main/java/com/example/demo1/swapcontroller.java
@@ -1,5 +1,155 @@
 package com.example.demo1;
 
-public class swapcontroller {
-    
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class swapcontroller implements Initializable {
+
+    @FXML
+    private MenuItem EURswap;
+
+    @FXML
+    private MenuItem EURto;
+
+    @FXML
+    private MenuItem GBPswap;
+
+    @FXML
+    private MenuItem GBPto;
+
+    @FXML
+    private MenuItem TOMANswap;
+
+    @FXML
+    private MenuItem TOMANto;
+
+    @FXML
+    private MenuItem USDswap;
+
+    @FXML
+    private MenuItem USDto;
+
+    @FXML
+    private MenuItem YENswap;
+
+    @FXML
+    private MenuItem YENto;
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private MenuButton menuswap;
+
+    @FXML
+    private Button prof;
+
+    @FXML
+    private Button swap;
+
+    @FXML
+    private Button swapbutton;
+
+    @FXML
+    private Label time;
+
+    @FXML
+    private Button transfer;
+
+    @FXML
+    private TextField vorrodi;
+
+    @FXML
+    void menuto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void swapEUR(ActionEvent event) {
+
+    }
+
+    @FXML
+    void swapGBP(ActionEvent event) {
+
+    }
+
+    @FXML
+    void swapTOMAN(ActionEvent event) {
+
+    }
+
+    @FXML
+    void swapUSD(ActionEvent event) {
+
+    }
+
+    @FXML
+    void swapYEN(ActionEvent event) {
+
+    }
+
+    @FXML
+    void swaparz(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toEUR(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toGBP(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toTOMAN(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toUSD(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toYEN(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tohome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toprof(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toswap(ActionEvent event) {
+
+    }
+
+    @FXML
+    void totransfer(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
