abfa96fd3e1bf978fde51f86197c73b1e1b029f9
amirhossein.abolfazli <amirhosseinabolfazli404@gmail.com>
2024-07-09 12:03:13 +0330

    admin

--- demo1/src/main/java/com/example/demo1/HelloApplication.java
+++ demo1/src/main/java/com/example/demo1/HelloApplication.java
@@ -12,7 +12,6 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 390, 440);
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Binance");
         stage.setScene(scene);
