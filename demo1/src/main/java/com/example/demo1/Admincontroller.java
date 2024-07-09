62b762c55118e81e9efda5de16e2ce13a73fbcfd
amirhossein.abolfazli <amirhosseinabolfazli404@gmail.com>
2024-07-09 08:41:10 +0330

    admin page

---
+++ demo1/src/main/java/com/example/demo1/Admincontroller.java
@@ -0,0 +1,25 @@
+package com.example.demo1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class Admincontroller {
+    @FXML
+    private TextField balance;
+
+    @FXML
+    private Button close;
+
+    @FXML
+    private Button open;
+
+    @FXML
+    private TextField status;
+    public void openBazzar(ActionEvent actionEvent) {
+    }
+
+    public void closeBazzar(ActionEvent actionEvent) {
+    }
+}
